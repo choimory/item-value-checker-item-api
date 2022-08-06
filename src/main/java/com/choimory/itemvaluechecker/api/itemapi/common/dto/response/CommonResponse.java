66a9bd0ae1e5de074bb13142c700a1d6ed907506
choimory <choimory@naver.com>
2022-08-07 02:52:45 +0900

    feat: 공용 자원 추가

---
+++ src/main/java/com/choimory/itemvaluechecker/api/itemapi/common/dto/response/CommonResponse.java
@@ -0,0 +1,15 @@
+package com.choimory.itemvaluechecker.api.itemapi.common.dto.response;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Builder
+@Getter
+@RequiredArgsConstructor
+public class CommonResponse<T> {
+    @Builder.Default
+    private final int status = 0;
+    private final String message;
+    private final T data;
+}
