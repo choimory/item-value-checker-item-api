67eaafe8823708a26f789df025777f27cdb60e42
choimory <choimory@naver.com>
2022-08-16 10:16:53 +0900

    feat: 요청, 응답 객체 추가

---
+++ src/main/java/com/choimory/itemvaluechecker/api/itemapi/item/dto/request/ItemRegistRequest.java
@@ -0,0 +1,4 @@
+package com.choimory.itemvaluechecker.api.itemapi.item.dto.request;
+
+public class ItemRegistRequest {
+}
---
+++ src/main/java/com/choimory/itemvaluechecker/api/itemapi/item/dto/response/ItemViewResponse.java
@@ -0,0 +1,26 @@
+package com.choimory.itemvaluechecker.api.itemapi.item.dto.response;
+
+import com.choimory.itemvaluechecker.api.itemapi.item.controller.ItemController;
+import com.choimory.itemvaluechecker.api.itemapi.item.dto.dto.ItemDto;
+import com.fasterxml.jackson.annotation.JsonUnwrapped;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.hateoas.RepresentationModel;
+import org.springframework.hateoas.server.mvc.WebMvcLinkBuilder;
+
+@Builder
+@NoArgsConstructor
+@Getter
+public class ItemViewResponse extends RepresentationModel<ItemViewResponse> {
+    @JsonUnwrapped
+    private ItemDto item;
+
+    public ItemViewResponse(ItemDto item) {
+        this.item = item;
+        add(WebMvcLinkBuilder.linkTo(ItemController.class)
+                .slash(item.getId())
+                .withSelfRel());
+    }
+}
